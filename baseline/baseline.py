cf4d6c6e0e0a2e218fa67931c1eae30be5164a6a
Philip Junker <philipjunker@gmail.com>
2018-05-07 12:09:57 +0200

    refactoring and highscore submission

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -7,33 +7,40 @@ import pickle
 import time
 import datetime
 
-DIM = 100
-TRAINING_DATA_POS = '../data/train_pos_full.txt'
-TRAINING_DATA_NEG = '../data/train_neg_full.txt'
-TEST_DATA = '../data/test_data.txt'
-VERBOSE = False
-MAX_DEPTH = 10
+''' global configuration '''
+DIM = 200       # Dimension of embeddings. Possible choices: 25, 50, 100, 200
+TRAINING_DATA_POS = '../data/train_pos_full.txt'    # Path to positive training data
+TRAINING_DATA_NEG = '../data/train_neg_full.txt'    # Path to negative training data
+TEST_DATA = '../data/test_data.txt'                 # Path to test data (no labels, for submission)
+VERBOSE = False                                     # Want lots of info on terminal?
+MAX_DEPTH = 20                                      # Max-depth for RandomForest Classifier
 
 def load_embeddings():
+    '''
+        Loads the pretrained glove twitter embeddings with DIM dimensions.
+        Returns a vector of embeddings and a word->idx dictionary
+        which returns the index of a word in the embeddings vector
+    '''
+
     word2idx = { } # dict to convert token to weight-idx
     weights = []
 
-    with open('../data/glove.twitter.27B/glove.twitter.27B.100d.txt', 'r') as file:
+    with open('../data/glove.twitter.27B/glove.twitter.27B.{}d.txt'.format(DIM), 'r') as file:
         for index, line in enumerate(file):
             values = line.split()       # word and weights are separated by space
             word = values[0]            # word is first element on each line
             word_weights = np.asarray(values[1:], dtype=np.float32) # weights for current word
             word2idx[word] = index
             weights.append(word_weights)
-            
-            #if index + 1 == 40000:     # for now only take 40k most frequent words
-            #    break
 
     return word2idx, weights
-
-
-# generate tweet embedding by averaging embeddings of all containted words
+ 
 def tweet_embedding(tweet, word2idx, embeddings):
+    '''
+        Returns tweet embedding by averaging embeddings of all contained words
+        Words which are not contained in vocabulary are ignored
+    '''
+
     vec_list = []
     tokens = tweet.split()      # split tweet by whitespaces
 
@@ -55,6 +62,8 @@ def tweet_embedding(tweet, word2idx, embeddings):
     return np.mean(vec_list, axis=0)
 
 def load_trainingdata(word2idx, embeddings):
+    ''' Loads and returns training data embeddings X and correct labels y in a randomized order '''
+
     train = []
     for tweet in open(TRAINING_DATA_POS, 'r', encoding='utf8'):
         train.append((tweet_embedding(tweet, word2idx, embeddings), 1))
@@ -62,11 +71,12 @@ def load_trainingdata(word2idx, embeddings):
     for tweet in open(TRAINING_DATA_NEG, 'r', encoding='utf8'):
         train.append((tweet_embedding(tweet, word2idx, embeddings), -1))
     
-    random.shuffle(train)   # shuffle order of training data randomly
-
-    return train
+    random.shuffle(train)       # shuffle order of training data randomly
+    return zip(*train)          # return embeddings and labels as two separate vectors
 
 def load_testdata(word2idx, embeddings):
+    ''' Loads and returns test data as a vector of embeddings '''
+
     test = []
     for line in open(TEST_DATA, 'r', encoding='utf8'):
         split_idx = line.find(',')  # first occurrence of ',' is separator between id and tweet
@@ -75,7 +85,8 @@ def load_testdata(word2idx, embeddings):
     return test
 
 def generate_submission(labels, filename):
-    
+    ''' Creates a submission file named according to filename in the current folder. '''
+
     with open(filename, 'w') as file:
         file.write('Id,Prediction\n')
         for i, label in enumerate(labels):
@@ -83,23 +94,22 @@ def generate_submission(labels, filename):
 
 
 def main():
+    ''' main entry point of application '''
+
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     word2idx, embeddings = load_embeddings()
-    # print(tweet_embedding("<user> i could actually kill that girl i'm so sorry ! ! !", word2idx, embeddings))
-    training_data = load_trainingdata(word2idx, embeddings)
-    pickle.dump( word2idx, open('word2idx.pkl', 'wb'))
-    pickle.dump( embeddings, open('embeddings.pkl', 'wb'))
-    X, y = zip(*training_data)
+    X, y = load_trainingdata(word2idx, embeddings)
+    pickle.dump( word2idx, open('word2idx_{}.pkl'.format(timestamp), 'wb'))
+    pickle.dump( embeddings, open('embeddings_{}.pkl'.format(timestamp), 'wb'))
     clf = RandomForestClassifier(max_depth=MAX_DEPTH, random_state=0)
-    scores = cross_val_score(clf, X, y, cv=5)
+    scores = cross_val_score(clf, X, y, cv=5)                   # calculate cross validation score using 5 splits
     print('cross validation scores calculated ({}-dimensional embeddings, max_depth={})'.format(DIM, MAX_DEPTH))
     print(scores)
-    clf.fit(X, y)
-    pickle.dump( clf, open('random_forest_classifier' ,'wb'))
-    # clf = pickle.load(open('random_forest_classifier', 'rb'))
+    clf.fit(X, y)                                               # train classifier on whole dataset
+    pickle.dump( clf, open('random_forest_classifier_MaxDepth{}_DIM{}_{}'.format(MAX_DEPTH, DIM, timestamp) ,'wb'))   # store trained classifier
     testdata = load_testdata(word2idx, embeddings)
-    submission_labels = clf.predict(testdata)
-    generate_submission(submission_labels, 'submission_{}.csv'.format(timestamp))
+    submission_labels = clf.predict(testdata)                   # predict labels of test data
+    generate_submission(submission_labels, 'submission_randomForest_MaxDepth{}_DIM{}_{}.csv'.format(MAX_DEPTH, DIM, timestamp))
 
 if __name__ == '__main__':
     main()
